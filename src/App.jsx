d85726b5c527430949333280b9f6110959cc651e
Ezedélio Garcia <96814597+Ezedcode@users.noreply.github.com>
2022-09-28 17:22:07 +0100

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from "react";
-import Content from "./data/Content.json";
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
+import { GlobalStyle } from "./theme/globalStyle";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100%;
+  max-width: 1440px;
+  background-color: red;
+`;
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Container></Container>
+      <Footer />
+      <GlobalStyle />
+    </div>
+  );
 };
 
 export default App;
